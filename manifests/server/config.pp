d610c22ebaf2170801e83dd7ddeca39334ad2026
Leon Bogaert <leon@tim-online.nl>
2013-04-18 21:28:38 +0200

    Accept GIT environmental variables

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -1,11 +1,20 @@
 class openssh::server::config {
 
-	augeas { "/etc/ssh/sshd_config":
-		context => '/files/etc/ssh/sshd_config',
-		changes => [
-			'set PermitRootLogin no',
-		],
-		notify  => Class['openssh::server::service'],
-	}
+    augeas { "/etc/ssh/sshd_config/PermitRootLogin":
+        context => '/files/etc/ssh/sshd_config',
+        changes => [
+            'set PermitRootLogin no',
+        ],
+        notify  => Class['openssh::server::service'],
+    }
+
+    augeas { "/etc/ssh/sshd_config/AcceptEnv":
+        context => '/files/etc/ssh/sshd_config',
+        onlyif  => 'match AcceptEnv/*[.="GIT_*"] size == 0',
+        changes => [
+            'set AcceptEnv/01 "GIT_*"',
+        ],
+        notify  => Class['openssh::server::service'],
+    }
 
 }
